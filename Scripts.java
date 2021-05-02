a0125f8ca01f8e7e1b18ec20b794f2dad1ea6ea5
harsha <harshavardhang70@gmail.com>
2021-05-02 19:59:38 +0530

    pom framework

---
+++ Scripts.java
@@ -0,0 +1,21 @@
+package pom_framework;
+
+import pom_framework.Functions;
+
+
+
+public class Scripts {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+		Functions ff = new Functions();
+		ff.mouseover();
+		ff.dropdown();
+		ff.checkbox();
+	
+		
+		
+	}
+
+}
